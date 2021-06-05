1670f4354a69d3b02f41f5cb291360a099a0a4c6
Mark <markdijkstra.eu@gmail.com>
2021-06-05 21:06:41 +0200

    code clean up

--- php/ArrayHelpers.php
+++ php/ArrayHelpers.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace common\components;
-
 /**
  *  Custom build array helper methods.
- *
  */
-class ArrayHelpers{
+class ArrayHelpers
+{
 
     /**
      *  Checks if key contains a certain value.
@@ -16,36 +14,24 @@ class ArrayHelpers{
      *  @param   string   $mtch   The value to match.
      *  @return  bool             Returns bool.
      */
-    public function multiKeyExists( $arr , $key = '' , $mtch = '' ){
-
-        if( is_array( $arr ) ){
-
+    public function multiKeyExists( $arr , $key = '' , $mtch = '' )
+    {
+        if (is_array( $arr )) {
             // is in base array?
-            if( array_key_exists( $key , $arr ) && $arr[$key] == $mtch || empty($mtch) ){
-
-                return true;
-
+            if (array_key_exists( $key , $arr ) && $arr[$key] == $mtch || empty($mtch) ) {
+                return;
             }
 
             // check arrays contained in this array
-            foreach( $arr as $element ){
-
-                if( is_array( $element ) ){
-
-                    if( self::multiKeyExists( $element , $key , $mtch ) ){
-
-                        return true;
-
+            foreach ($arr as $element) {
+                if (is_array( $element )) {
+                    if (self::multiKeyExists( $element , $key , $mtch ) ) {
+                        return;
                     }
-
                 }
-
             }
-
         }
-
         return false;
-
     }
 
     /**
@@ -57,16 +43,12 @@ class ArrayHelpers{
      *  @param    string   $val   The value to set.
      *  @return   array           Returns the new array.
      */
-    public function setValueByKey( $arr , $key , $ind , $val ){
-
+    public function setValueByKey($arr , $key , $ind , $val)
+    {
         if( is_array( $arr ) && $ind < count( $arr ) ){
-
             $arr[$ind][$key] = $val;
-
         }
-
         return $arr;
-
     }
 
     /**
@@ -77,24 +59,16 @@ class ArrayHelpers{
      *  @param    string   $val   The value to set.
      *  @return   array           Returns the new array.
      */
-    public function setValueByEmptyKey( $arr , $key , $val ){
-
-        if( is_array( $arr ) ){
-
-            foreach( $arr as &$item ){
-
-                if( $item[$key] === "" || $item[$key] === " " || $item[$key] === false || $item[$key] === null ){
-
+    public function setValueByEmptyKey($arr , $key , $val)
+    {
+        if (is_array( $arr )) {
+            foreach ($arr as &$item) {
+                if ($item[$key] === "" || $item[$key] === " " || $item[$key] === false || $item[$key] === null) {
                     $item[$key] = $val;
-
                 }
-
             }
-
         }
-
         return $arr;
-
     }
 
     /**
@@ -102,24 +76,18 @@ class ArrayHelpers{
      *
      *  @param    array    $arr    The array to search.
      *  @param    string   $key    The key to search.
-     *  @param    string   $mtch   The value to match.
+     *  @param    string   $match   The value to match.
      *  @param    string   $val    The value to use.
      *  @return   array            Returns the new array.
      */
-    public function setDefaultValue( $arr , $key , $ind , $mtch , $val ){
-
-        if( $ind === false || $mtch == false ){
-
-            $arr = self::setValueByEmptyKey( $arr , $key , $val );
-
-        }elseif( !self::multiKeyExists( $arr , $key , $mtch ) ){
-
-            $arr = self::setValueByKey( $arr , $key , $ind , $val );
-
+    public function setDefaultValue($arr , $key , $ind , $match , $val) 
+    {
+        if ($ind === false || $mtch == false) {
+            $arr = self::setValueByEmptyKey($arr , $key , $val);
+        } elseif (!self::multiKeyExists($arr , $key , $match)) {
+            $arr = self::setValueByKey($arr, $key, $ind, $val);
         }
-
         return $arr;
-
     }
 
     /**
@@ -134,46 +102,26 @@ class ArrayHelpers{
      */
     public function cleanUpArray(array $input, $easy = false, $throwByFoundObject = true)
     {
-        if( $easy ){
-
-            $output = array_map(function($v){
-
-                return trim( strip_tags($v) );
-
+        if ($easy) {
+            $output = array_map(function($v) {
+                return trim(strip_tags($v));
             }, $input);
-
-        }else{
-
+        } else {
             $output = $input;
 
-            foreach( $output as $key => $value ){
-
-                if( is_string($value) ){
-
+            foreach ($output as $key => $value) {
+                if (is_string($value)) {
                     $output[$key] = preg_replace('/[^(\x20-\x7F)\x0A\x0D]*/', '', $value);
-
                     $output[$key] = trim(preg_replace('~[\r\n]+~', ' ', $value));
-
                     $output[$key] = html_entity_decode(mb_convert_encoding(stripslashes($value), "HTML-ENTITIES", 'UTF-8'));
-
                     $output[$key] = trim( strip_tags($value) );
-
-                }elseif( is_array($value) ){
-
+                } elseif (is_array($value)) {
                     $output[$key] = self::cleanUpArray($value);
-
-                }elseif( is_object($value) && $throwByFoundObject ){
-
+                } elseif (is_object($value) && $throwByFoundObject) {
                     throw new Exception('Object found in Array by key ' . $key);
-
                 }
-
             }
-
         }
-
         return $output;
-
     }
-
 }
